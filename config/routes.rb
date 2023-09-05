62530a16736371e399f92f4bf86121600142b845
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-09-05 12:48:27 +0430

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show, :new, :create ] do
+    resources :employees, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+      resources :attendances, only: [:index, :new, :create, :destroy]
+      resources :contracts, only: [:new, :create, :destroy]
+      resources :salaries, only: [:new, :create, :update, :destroy]
+      resources :addresses, only: [:new, :create, :update, :destroy]
+      resources :jobs_info, only: [:new, :create, :update, :destroy]
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :departments, only: [:index, :show, :new, :create, :update, :destroy] do
+    resources :projects, only: [:index, :show, :new, :create, :update, :destroy]
+    resources :reports, only: [:index, :show, :new, :create, :update, :destroy]
+  end
 end
